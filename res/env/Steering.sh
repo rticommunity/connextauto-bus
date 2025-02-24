fb3a825372a48bf382c9d2f539b407db9fc0bc3d
Rajive Joshi <rajive@rti.com>
2025-02-23 20:20:44 -0800

    Define Steering Column Controller use case

---
+++ res/env/Steering.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#******************************************************************************
+# (C) Copyright 2020-2025 Real-Time Innovations, Inc.  All rights reserved.
+#
+# The use of this software is governed by the terms specified in the
+# RTI Labs License Agreement, available at https://www.rti.com/terms/RTILabs.
+#
+# By accessing, downloading, or otherwise using this software, you agree to
+# be bound by those terms.
+#*****************************************************************************
+# USAGE:
+#   Source the environment variables into the current shell
+#     . Drive.sh
+#*****************************************************************************
+
+# --- Project Directory ---
+if [ "${DATABUSHOME}" = "" ]; then echo "DATABUSHOME Undefined!" ; return; fi
+
+# -- Base Environment ---
+[ -f $DATABUSHOME/res/env/QOS_PROVIDER.sh ] &&
+. $DATABUSHOME/res/env/QOS_PROVIDER.sh || NDDS_QOS_PROFILES=""
+
+
+# --- QoS Profiles ---
+
+# QoS: Services
+NDDS_QOS_PROFILES+=";$DATABUSHOME/res/qos/services/Drive_qos.xml"
+
+# QoS: Service Deployments
+NDDS_QOS_PROFILES+=";$DATABUSHOME/res/qos/services/drive/SteeringColumn_qos.xml"
+NDDS_QOS_PROFILES+=";$DATABUSHOME/res/qos/services/drive/SteeringController_qos.xml"
+NDDS_QOS_PROFILES+=";$DATABUSHOME/res/qos/services/drive/SteeringDisplay_qos.xml"
+
+
+# --- Data-Oriented Interfaces ---
+
+# Interfaces: Services
+NDDS_QOS_PROFILES+=";$DATABUSHOME/if/Steering.xml"
+
+# --- Show Environment ---
+echo NDDS_QOS_PROFILES=$NDDS_QOS_PROFILES
+#echo NDDS_QOS_PROFILES=$NDDS_QOS_PROFILES
